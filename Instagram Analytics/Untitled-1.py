ff7fd5f1780d5791b8dedffe0d734c64e9ac3958
Kaue Mandarino <kauemandarino@gmail.com>
2023-04-22 20:51:14 -0300

    Comentarios

---
+++ Instagram Analytics/Untitled-1.py
@@ -0,0 +1,389 @@
+
+from collections import Counter
+
+
+
+texto = ('''
+Pesquise assuntos, locais e fontes
+Página inicial
+Para você
+Seguindo
+Destaques Jornalísticos
+Brasil
+Mundo
+Local
+Negócios
+Ciência e tecnologia
+Entretenimento
+Esportes
+Saúde
+Suas notícias
+sábado, 22 de abril
+Hoje
+23 °
+13 °
+dom.
+22 °
+13 °
+seg.
+22 °
+13 °
+ter.
+23 °
+14 °
+
+Curitiba
+
+16 °C
+weather.com
+Principais notícias
+
+
+Lula assina acordos com Portugal e promete empenho por aliança entre União Europeia e Mercosul; veja lista
+3 horas atrás
+
+UOL
+Lula assina 13 acordos em Portugal, volta a atacar antecessores e comete gafe
+4 horas atrás
+
+G1
+Em Portugal, Lula é recebido pelo presidente Marcelo Rebelo e presta homenagem a Camões
+12 horas atrás
+
+Estado de Minas
+Presidente Lula na Europa e a CPMI já está a caminho
+15 horas atrás
+Opinião
+Cobertura completa
+
+
+Globo
+Jogo Fluminense 2 x 0 Athletico-PR melhores momentos - Campeonato Brasileiro, Rodada 2 - tempo real | ge
+3 horas atrás
+
+UOL Esporte
+Fluminense domina Athletico, tem golaço de Nino e vence outra no Brasileiro
+1 hora atrás
+
+Itatiaia
+Vice-artilheiro do Fluminense, Nino destaca trabalho de Diniz
+1 hora atrás
+
+Globo
+Athletico é dominado no primeiro tempo e perde para o Fluminense no Maracanã
+1 hora atrás
+Cobertura completa
+
+Lula nega ter igualado Rússia e Ucrânia na guerra
+
+8 horas atrás
+
+
+Polícia Federal vai ouvir mais nove militares do GSI neste domingo
+
+1 hora atrás
+
+Notícias locais
+
+Pit bull ataca idosa no Cajuru, é esfaqueado por filho da vítima e mulher tem resgate difícil
+
+5 horas atrás
+
+Filho de Tony Garcia é preso por tráfico de drogas
+
+1 hora atrás
+
+Curitiba começa a testar ônibus elétricos da BYD na próxima semana
+
+1 hora atrás
+
+Opções para você
+
+TSE multa advogado que fez petição com ChatGPT
+
+12 horas atrás
+
+
+Olhar Digital
+Ao Vivo | A incrível réplica da Lua criada pela Nasa! | 21/04/2023 | #OlharDigital
+
+23 horas atrás
+
+Multiverso Notícias
+Você consegue descobrir o erro nesta imagem em apenas 7s?
+
+11 horas atrás
+Seus assuntos
+Brasil
+
+Lula pede desculpas sobre fala em que diz que pessoas com transtornos mentais têm “desequilíbrio de parafuso”
+
+1 hora atrás
+
+
+Mezanino de restaurante cai e deixa turistas feridos no Capivari, em Campos do Jordão
+
+3 horas atrás
+
+
+Protesto contra aumento de passagem em BH é marcado para terça-feira (25/4)
+
+5 horas atrás
+
+Mundo
+
+Ucrânia ofusca parte da primeira agenda oficial de Lula em Portugal
+
+3 horas atrás
+
+
+Crise de Fernández: Argentina enfrenta disparada do dólar, da inflação e a maior seca desde 1929
+
+15 horas atrás
+
+
+Pizza flambada pode ter causado incêndio mortal em restaurante em Madri
+
+3 horas atrás
+
+Negócios
+
+Decisão do Supremo pode aumentar rendimento do FGTS: entenda
+
+8 horas atrás
+
+
+Mega-Sena acumulada: Veja os 5 números mais jogados por ganhadores da Mega neste ano
+
+8 horas atrás
+
+
+Edital Concursos Brasil
+Ministério da Fazenda divulga pacote com 13 medidas para estimular crédito e PPPs
+
+9 horas atrás
+
+Ciência e tecnologia
+
+SpaceX Starship: explosão de foguete não é o fracasso que parece, dizem especialistas
+
+5 horas atrás
+
+
+Whatsapp: veja como mensagens temporárias vão poder ser salvas para sempre em nova atualização
+
+Ontem
+
+
+Observatório registra queda de até 22 meteoros por hora durante pico da chuva de meteoros Líridas no RS; VÍDEO
+
+8 horas atrás
+
+Entretenimento
+
+Guerra no BBB 23: Equipe de Larissa acusa pai de Bruna Griphao de traição
+
+3 horas atrás
+
+
+Maíra Cardi é pedida em casamento por Thiago Nigro, quase dois meses após assumirem namoro
+
+5 horas atrás
+
+
+Irmã de Cristiano Ronaldo declara torcida no BBB23: "Guerreira"
+
+4 horas atrás
+
+Esportes
+
+Consistente, Fluminense vence o Athletico, no Maracanã, e mostra sua força no Brasileirão
+
+1 hora atrás
+
+
+Jovem Pan Esportes
+São Paulo x América-MG - AO VIVO - 22/04/2023 - Brasileirão
+
+3 horas atrás
+
+
+David Luiz é desfalque contra o Internacional, e Flamengo terá mudanças com Jorge Sampaoli
+
+4 horas atrás
+
+Para você
+Recomendações com base em seus interesses
+
+
+Bard ajuda a criar e codificar funções no Planilhas Google
+
+Ontem
+
+
+O cerco que a Inteligência Artificial (IA) faz a cada um de nós e a possível trapaça do ChatGPT
+
+Ontem
+Opinião
+
+Após decisão de Elon Musk, G1, O Globo, Estadão e outros jornais perdem selo de verificação do Twitter
+
+Ontem
+
+
+SP: operador que barrou malas com droga foi morto pelo PCC
+
+Ontem
+
+
+Protesto reúne motoristas de aplicativos em Porto Alegre
+
+2 dias atrás
+
+Maior inimigo de Sauron em O Senhor dos Anéis não eram os elfos, ou os magos
+
+2 dias atrás
+Fontes
+Jornal GGN
+OS DESTAQUES NO GGN
+POR LUIS NASSIF
+Economia: a busca sôfrega pela aprovação da Globo e do mercado
+
+POLÍTICA EXTERNA
+Lula reafirma laços entre Brasil e Portugal
+
+ANÁLISE
+CARF, entre a tragédia e a farsa
+
+
+Destaques
+1 hora atrás
+Jornal Correio
+Destaques do Dia
+VÍDEO
+Turistas da Romênia são agredidos durante assalto no Pelourinho
+
+WEB
+Influencer mostra rotina de um estudante de medicina do ProUni
+
+CINEMA
+Cine Clube faz refletir sobre a morte enquanto se come pipoca
+
+
+Destaques
+2 horas atrás
+Jornal da Cidade de Bauru
+Destaques
+DENGUE TIPO 1
+Dengue grave em Bauru intriga Saúde e Butantan estuda o vírus
+
+VIZINHA DE BAURU
+Iacanga: cidade-irmã de Bauru festeja 98 anos neste mês de abril
+
+ANTIGO CINE BAURU
+Mulher tem casa invadida cinco vezes e furtos assustam vizinhos
+
+
+Destaques
+11 horas atrás
+Jovem Pan
+DESTQUES DA MANHÃ
+Viagem a Portugal
+Lula afirma que Rússia e Ucrânia não querem fim da guerra
+
+Interino do GSI
+Cappelli diz que tornará públicos todos vídeos de 8 de Janeiro
+
+Golpe das bagagens
+Justiça manda soltar mulher presa por trocar etiquetas
+
+
+Destaques
+7 horas atrás
+AN - Jornal A Notícia
+ASSUNTOS DO DIA
+EM UM ANO
+Lei ambiental de Joinville registra liberação de 189 licenças
+
+ARAQUARI
+Acidente entre carro e caminhão deixa duas pessoas feridas na BR-280
+
+MUDANÇAS
+Plano Viário planeja Joinville pelos próximos 30 anos
+
+
+Destaques
+5 horas atrás
+Tribuna de Minas
+DIRETO DA REDAÇÃO
+MEIO AMBIENTE
+Retomada Puri: o conhecimento ancestral em prol da conservação
+
+MUNICÍPIO DE TOCANTINS
+Polícia apreende drogas escondidas na cueca de suspeito
+
+PETS
+Veja como cuidar de um porquinho-da-Índia em casa
+
+
+Destaques
+48 minutos atrás
+Diário do Rio de Janeiro
+Pombos fazem sujeira na estação da Praça XV
+Hotéis lotados
+Pesquisa aponta Miguel Pereira como destino preferido no feriado
+
+Nojo
+Pombos fazem sujeira em caixas de som na estação das Barcas
+
+Dói no bolso
+Vandalismo de placas custa aos cofres R$ 240 mil por ano
+
+
+Destaques
+7 horas atrás
+Diário
+Notícias de Santa Maria e região
+DISTRITO CRIATIVO
+Mais de 20 empreendimentos fazem parte do cenário da Vila Belga
+
+DESTAQUE
+Jovem ex-Riograndense assina contrato com o Grêmio
+
+PROGRAMAÇÃO
+Cinco filmes da Netflix para ver durante o final de semana
+
+
+Destaques
+5 horas atrás
+Ver mais fontes
+Checagem de fatos
+De fontes independentes
+
+
+Lula não disse que vai suspender mais de um milhão de beneficiários do INSS
+Declaração: Lula disse que vai suspender mais de um milhão de beneficiários do INSSChecagem de fatos por UOL Notícias: Falso
+9 horas atrás
+
+Vídeo mostra ação do Ibama em 2020, não ameaça a produtor no governo Lula
+Declaração: Fiscal do Ibama ameaça produtor rural durante o governo LulaChecagem de fatos por Bol - Uol: Falso
+9 horas atrás
+
+Foto mostra Lula com funcionários do banco dos Brics e não só comitiva
+Declaração: Foto mostra centenas de membros da comitiva de Lula na ChinaChecagem de fatos por UOL Notícias: Falso
+Ontem
+
+É falso que Lula chamou apoiadores de jumentos
+Declaração: Lula chamou apoiadores de jumentosChecagem de fatos por Aos Fatos: falso
+4 dias atrás
+Receba mais notícias com base nos seus interesses''')
+
+palavras = texto.split()
+frequencias = Counter(palavras)
+
+
+print("Frequências:")
+for palavra, frequencia in sorted(frequencias.items(), key=lambda x: x[1], reverse=True):
+    if frequencia > 5:
+        print(f"{palavra}: {frequencia}")
+        
